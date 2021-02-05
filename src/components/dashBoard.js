68a60e41612029331b1d2854ebffded7dfc5170e
Roger Mai <cab022010@coderacademy.edu.au>
2021-02-05 11:07:44 +1000

    frontend

---
+++ src/components/dashBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DashBoard = props => {
+    return (
+        <div>
+            <div>
+                <h1>DashBoard</h1>
+                <h1>Status: {this.props.loggedInStatus}</h1>
+            </div>
+        </div>
+    );
+};
+
+export default DashBoard;
